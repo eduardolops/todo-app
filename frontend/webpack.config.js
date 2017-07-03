988be762633beb716f06595f2dad7725ac87be4f
Eduardo Lopes <edulopestj@gmail.com>
2017-07-03 13:01:47 -0300

    modificando o nome do render do js na view

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/index.jsx',
     output: {
         path: __dirname + '/public',
-        filename: './bundler.js'
+        filename: './app.js'
     },
     devServer: {
         port: 8080,
