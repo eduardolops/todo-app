4dda2cc4f9556e9029c5e680f6a6f80b15b91426
Eduardo Lopes <edulopestj@gmail.com>
2017-07-03 15:25:38 -0300

    creating template menu for navigation at the app

---
+++ frontend/src/template/menu.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default props => (
+    <nav className="navbar navbar-inverse bg-inverse">
+        <div className="container">
+            <div className="navbar-header">
+                <a className="navbar-brand" href="">
+                    <i className="fa fa-calendar-check-o"></i> Todo
+                </a>
+            </div>
+            <div id="navbar" className="navbar-collapse collapse">
+                <ul className="nav navbar-nav">
+                    <li><a href="#/todos">Tarefas</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+)
