33e280b0d6917f38656a676053c65c7e052b0ceb
Eduardo Lopes <edulopestj@gmail.com>
2017-07-04 22:58:16 -0300

    add onChange

--- frontend/src/todo/form.jsx
+++ frontend/src/todo/form.jsx
@@ -5,7 +5,9 @@ import IconButton from '../template/iconButton'
 export default props => (
     <div role="form" className="todoForm row">
         <Grid cols="12 9 10">
-            <input id="description" className="form-control" placeholder="Add a task"/>
+            <input id="description" className="form-control" placeholder="Add a task" 
+            onChange={props.handleChange}
+            value={props.description} />
         </Grid>
         <Grid cols="12 3 2">
             <IconButton style="primary" icon="plus" onClick={props.handleAdd} />
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -7,18 +7,26 @@ export default class Todo extends Component {
 
     constructor(props){
         super(props)
+        this.state = { description: '', list: [] }
+        this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
     }
 
+    handleChange(e){
+        this.setState({ ...this.state, description: e.target.value })
+    }
+
     handleAdd(){
-        console.log(this)
+        console.log(this.state.description)
     }
     
     render(){
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro" />
-                <Form handleAdd={this.handleAdd}/>
+                <Form handleAdd={this.handleAdd}
+                      handleChange={this.handleChange}
+                      description={this.state.description}/>
                 <List />
             </div>
         )
