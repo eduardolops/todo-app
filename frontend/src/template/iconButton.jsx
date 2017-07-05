f9cab23742febecd13eec7af097e7c2f2b9fde1d
Eduardo Lopes <edulopestj@gmail.com>
2017-07-04 22:45:55 -0300

    add onClick in the button

--- frontend/src/template/iconButton.jsx
+++ frontend/src/template/iconButton.jsx
@@ -3,7 +3,7 @@ import If from './helpers/If'
 
 export default props => (
     <If test={!props.hide}>
-        <button className={'btn btn-' + props.style} onclick={props.onclick}>
+        <button className={'btn btn-' + props.style} onClick={props.onClick}>
             <i className={'fa fa-' + props.icon}></i>
         </button>
     </If>
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,12 +4,21 @@ import Form from './form'
 import List from './list'
 
 export default class Todo extends Component {
+
+    constructor(props){
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd(){
+        console.log(this)
+    }
     
     render(){
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro" />
-                <Form />
+                <Form handleAdd={this.handleAdd}/>
                 <List />
             </div>
         )
