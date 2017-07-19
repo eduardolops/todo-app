848efccde7b07e26500c5a07a950080ac7257cc6
Eduardo Lopes <edulopestj@gmail.com>
2017-07-19 15:35:00 -0300

    add class component todoForm

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -1,4 +1,16 @@
+import axios from 'axios'
+
+const URL = 'http://127.0.0.1:3003/api/todo'
+
 export const changeDescription = event => ({
     type: 'DESCRIPTION_CHANGED',
     payload: event.target.value
 })
+
+export const search = event => {
+    request = axios.get(`${URL}?sort=-createdAt`)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
